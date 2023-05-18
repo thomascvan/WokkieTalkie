c0550477ad61254d6ced1cd70a9927125e369a05
Thomas Van <thomascvan11@gmail.com>
2023-05-17 23:53:45 -0700

    added SafeAreaView

--- App.js
+++ App.js
@@ -6,16 +6,20 @@ import ViewImageScreen from './app/screens/ViewImageScreen'
 
 export default function App() {
   return (
-      // <WelcomeScreen />
-      <ViewImageScreen />
+    <SafeAreaView style={styles.container}>
+      <WelcomeScreen />
+      {/* <ViewImageScreen /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // width: '100%',
+    // height: '100%',
     backgroundColor: 'white',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
 });
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, SafeAreaView } from 'react-native';
 
 
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
+      <View style={styles.closeIcon}><Text>X</Text></View>
       <Image resizeMode="contain" style={styles.image} source={require("../assets/zaku-diagram.jpg")} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  closeIcon: {
+    width: 40,
+    height: 40,
+    top: 20,
+    left: 20,
+    backgroundColor: 'gray',
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   container: {
     backgroundColor: 'black',
     flex: 1
